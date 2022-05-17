3a57eb406310c6e16cd7d8f238eef2e541ee5557
christian <krikoacaso@gmail.com>
2022-05-17 15:30:42 -0400

    descripcion

--- js/calculate.js
+++ js/calculate.js
@@ -1,5 +1,8 @@
 function calcular(){
     document.getElementById("loading").style.display="block";
+    let porcion1,porcion2,porcion3,porcion4,porcion5,porcion6;
+    porcion1=document.getElementById("porcion1").value;
+    console.log("porcion1",porcion1);
     setTimeout(function(){
         document.getElementById("loading").style.display="none";
         document.getElementById("resultado").innerHTML="700"+"Bs.";
