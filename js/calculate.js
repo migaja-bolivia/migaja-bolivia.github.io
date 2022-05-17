4842b1dfbe8d4379260459bc00da2e958bc74f0d
christian <krikoacaso@gmail.com>
2022-05-17 14:52:37 -0400

    descripcion

--- js/calculate.js
+++ js/calculate.js
@@ -1,3 +1,12 @@
 function calcular(){
-    document.getElementById("resultado").innerHTML="700"+"Bs.";
+    document.getElementsByClassName("loader").style.display="block";
+    setTimeout(function(){
+        document.getElementsByClassName("loader").style.display="none";
+        document.getElementById("resultado").innerHTML="700"+"Bs.";
+    },3000)
+    
+};
+
+window.onload=()=>{
+    document.getElementsByClassName("loader").style.display="none";
 };
